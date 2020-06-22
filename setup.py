69889e7a39eb1a8f6b2a3d59d8f3dafa24bc63fb
Shreyas . Potdar <47130139+SHREYAS1188@users.noreply.github.com>
2020-06-22 18:05:22 +0530

    setup upload

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+
+def readme():
+    with open('README.md') as f:
+        README = f.read()
+    return README
+
+
+setup(
+    name="myvectors",
+    packages=["myvectors"],
+    version="0.1",
+    description=" Exploring VECTOR Mathematics ",
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/SHREYAS1188/vector_python_package",
+    author="Shreyas Potdar",
+    author_email="shreyasmp2001@gmail.com",
+    license="MIT",
+    include_package_data=True,
+    install_requires=["math"],
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 2.8",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5"
+        "Programming Language :: Python :: 3.6"
+        "Programming Language :: Python :: 3.7",
+    ],
+)
