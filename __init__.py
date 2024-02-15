71b2a73de3d56d884b0e9d2247249660bd854b7a
Shreyas . Potdar <47130139+SHREYAS1188@users.noreply.github.com>
2024-02-15 13:44:02 +0530

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,5 @@
+from math import sqrt,acos,degrees
+from mpl_toolkits.mplot3d import axes3d   # used in draw_vector function 
+import matplotlib.pyplot as plt   # used in draw_vector function
+import numpy as np
+from myvectors.myvectors import mag,dot,cross,projection,angle,trianglearea,sectionsutram,collinear3,s_triplepro,v_triplepro,draw_vector,direction_Cosine
